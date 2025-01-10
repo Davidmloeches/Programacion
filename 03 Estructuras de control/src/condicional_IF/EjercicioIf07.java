04988b091f63814978d9a4cc387a5845c520d9b1
Davidmloeches <davidsanfermls@gmail.com>
2025-01-10 13:58:50 +0100

    Todos los ej de programacion

---
+++ 03 Estructuras de control/src/condicional_IF/EjercicioIf07.java
@@ -0,0 +1,14 @@
+package condicional_IF;
+/**
+ * A las marmotas les gusta organizar fiestas, y en sus fiestas les gusta comer tazas de mantequilla de cacahuete. Pero no debe haber muchas, porque si no enferman. Una fiesta de la marmota exitosa tiene entre 10 y 20 tazas de mantequilla de cacahuete, ambas inclusive, a menos que sea fin de semana, en cuyo caso necesitarán de 15 a 25 tazas.
+ * Escribe un programa que lea dos valores:
+ * El primero es el número de tazas de mantequilla de cacahuete que hay en la fiesta.
+ * El segundo es un booleano que indica si es fin de semana.
+ * El programa debe imprimir si la fiesta tiene éxito o no (true o false).
+ * */
+public class EjercicioIf07 {
+    public static void main(String[] args) {
+
+
+    }
+}
